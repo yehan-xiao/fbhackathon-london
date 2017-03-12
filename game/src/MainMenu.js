8cb94fa014c3b154c18ac8801aec8eebaa259372
Yehan <xiaoyehan96@outlook.com>
2017-03-12 03:33:45 +0000

    Squab

--- game/src/MainMenu.js
+++ game/src/MainMenu.js
@@ -10,6 +10,14 @@ Candy.MainMenu.prototype = {
 	},
 	startGame: function() {
 		// start the Game state
+
 		this.state.start('Game');
 	}
+function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=230721050727949";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk');
 };
